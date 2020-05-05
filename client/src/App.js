ff848a95223c274e8b6e756c59087c2cb13135a4
Willie Wright Jr <admin@razorwebs.com>
2020-05-05 11:17:04 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import Header from "./components/header/Header.js";
+
+function App() {
+  return <div className="App">Test</div>;
+}
+
+export default App;
---
+++ client/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default Header = () {
+    render (){
+        return (
+            <div className="header">
+                <div>test</div>
+            </div>
+        )
+    }
+}
