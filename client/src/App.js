b750c43702fa2875d1e4bb56dc6d9947786020bb
Willie Wright Jr <admin@razorwebs.com>
2020-05-05 16:29:16 -0700

    Updated init

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 // import Header from "./components/header/Header.js";
 
 function App() {
-  return <div className="App">Test</div>;
+  return <div className="App">I love to eat pussy!</div>;
 }
 
 export default App;
